7c377f17eca68b436bb7d1a5046157a2ce5fec03
yasu0228 <yasushi.tanimori@gtec-oka.com>
2018-09-04 20:01:13 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -5,14 +5,13 @@ import time
 import numpy as np
 import cv2
 
-
-# Create a black image
-img = np.zeros((512,512,3), np.uint8)
-
 # Draw a diagonal blue line with thickness of 5 px
 cv2.line(img,(0,0),(511,511),(255,0,0),5)
 
 class FaceDetector(object):
+    # Create a black image
+    img = np.zeros((512,512,3), np.uint8)
+    
     def __init__(self, flip = True):
         self.vs = PiVideoStream(resolution=(800, 608)).start()
         self.flip = flip
